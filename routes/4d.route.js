02856c09e9e145c58c43d8fe612387a97a58c7fb
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2023-05-01 15:25:10 +0300

    first commit

--- routes/4d.route.js
+++ routes/4d.route.js
@@ -4,7 +4,7 @@ const fourDmodel = require("../model/4d.model");
 const add4dMiddleware = require("../middleware/add4d.middleware");
 
 app.get('/all4d', async (req, res) => {
-  let data = await fourDmodel.find();
+  let data = await fourDmodel.find().populate('clientID');
   if (data.length != 0) {
     res.json(data);
   } else {
@@ -26,6 +26,7 @@ app.post("/add4d", add4dMiddleware, async (req, res, next) => {
       let fourDproject = await fourDmodel.insertMany({
         name: clientName,
         clientID:id,
+        fourDphotos,
         videoLink,
       });
       res.json({ message: "success", data: fourDproject[0] });
